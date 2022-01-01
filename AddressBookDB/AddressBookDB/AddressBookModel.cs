639ae7a77f576a2de0620d1a4f835d4b962df022
Your UserName <aratinaik151997@gmail.com>
2022-01-01 08:36:23 +0530

    Added Table AddressBook and properties to model class

---
+++ AddressBookDB/AddressBookDB/AddressBookModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookDB
+{
+    class AddressBookModel
+    {
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string Address { get; set; }
+        string City { get; set; }
+        string State { get; set; }
+        string Zip { get; set; }
+        string PhoneNumber { get; set; }
+        string Email { get; set; }
+
+    }
+}
---
+++ AddressBookDB/SQLQuery1.sql
@@ -0,0 +1,4 @@
+﻿create database AddressBookServiceDB
+
+use AddressBookServiceDB
+
