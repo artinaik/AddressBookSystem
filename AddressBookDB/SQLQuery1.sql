191fe915f90832c732e9a63173d4cb8619bf3ef2
Your UserName <aratinaik151997@gmail.com>
2022-01-01 08:39:04 +0530

    Added Table AddressBook and properties to model class

---
+++ AddressBookDB/SQLQuery1.sql
@@ -0,0 +1,17 @@
+﻿create database AddressBookServiceDB
+
+use AddressBookServiceDB
+
+create table AddressBook
+(FirstName varchar(100),
+LastName varchar(100),
+Address varchar(100),
+City varchar(100),
+State varchar(100),
+Zip varchar(100),
+PhoneNumber varchar(100),
+Email varchar(100),
+)
+
+select * from AddressBook
+
