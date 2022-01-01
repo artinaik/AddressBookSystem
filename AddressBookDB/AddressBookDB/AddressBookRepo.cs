2fe962cfdbe6fa564c989a518e489f5cd46a1074
Your UserName <aratinaik151997@gmail.com>
2022-01-01 08:23:54 +0530

    Added code to connect database AddressBookServiceDB

---
+++ AddressBookDB/AddressBookDB/AddressBookRepo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace AddressBookDB
+{
+    class AddressBookRepo
+    {
+        public static string connectionString = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=AddressBookServiceDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"; //"Data Source=(localdb)\ProjectsV13;Initial Catalog=payroll_service;Integrated Security=True";
+        SqlConnection connection = new SqlConnection(connectionString);
+
+    }
+}
